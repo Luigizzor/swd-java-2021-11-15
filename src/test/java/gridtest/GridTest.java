0d6bf9a44c9282b46a81e4fcbfa41be65a488cab
Luigizzor <luigizzor@gmail.com>
2021-11-16 16:41:15 +0100

    grid hozzaadva

--- src/test/java/gridtest/GridTest.java
+++ src/test/java/gridtest/GridTest.java
@@ -10,7 +10,7 @@ import java.net.URL;
 public class GridTest {
     @Test
     void gridTest(WebDriver driver, URL url){
-        driver.get(url +"/grid");
+        driver.get(url + "/grid");
 
     }
 
